5531d36271ed2ef0d2154a4526ed6e7ff5c2dd3f
Azkia <azisarf9@gmail.com>
2019-07-24 11:14:28 +0800

    push lagi non crm

---
+++ resources/views/manager_non_crm/dashboard_manager_non_crm.blade.php
@@ -0,0 +1,72 @@
+@extends('layouts_users.app_manager_non_crm')
+
+@section('content_header')
+<div class="row">
+    <div class="col-md-12">
+        <div class="panel block">
+            <div class="panel-body">
+                <h1> Dashboard Manager Non CRM</h1>
+                <ol class="breadcrumb">
+                    <li><i class="fa fa-dashboard"></i> Home</li>
+                    
+                </ol>
+            </div>
+        </div>
+    </div>
+</div>
+@endsection
+
+@section('content')
+<div class="row">
+    <div class="col-md-12">
+        <div class="panel block">
+            <div class="panel-body">
+                {{-- <div class="card-body">
+                    @if (session('status'))
+                        <div class="alert alert-success" role="alert">
+                            {{ session('status') }}
+                        </div>
+                    @endif
+                </div> --}} 
+                <h2>Daftar Laporan yang tersedia</h2>
+                {{----------- box -----------}}
+                <a href="{{asset('/manager_non_crm/manager_non_crm_customer')}}">
+                    <div class="col-md-4">
+                        <div class="info-box bg-red col-md-4">
+                        <span class="info-box-icon"><i class="fa fa-envelope-o"></i></span>
+                            <div class="info-box-content">
+                                <span class="info-box-text">Laporan Customer</span>
+                                <span class="info-box-number">{{$customers}}</span>
+                            </div>     
+                        </div>
+                    </div>
+                </a>
+                <a href="{{asset('/manager_non_crm/manager_non_crm_kontrak')}}">
+                    <div class="col-md-4">
+                        <div class="info-box bg-blue col-md-4">
+                        <span class="info-box-icon"><i class="fa fa-envelope-o"></i></span>
+                            <div class="info-box-content">
+                                <span class="info-box-text">Laporan kontrak</span>
+                                <span class="info-box-number">{{$kontrak}}</span>
+                            </div>     
+                        </div>
+                    </div>
+                </a>
+                <a href="#">
+                        <div class="col-md-4">
+                            <div class="info-box bg-orange col-md-4">
+                            <span class="info-box-icon"><i class="fa fa-envelope-o"></i></span>
+                                <div class="info-box-content">
+                                    <span class="info-box-text">Laporan data MOU</span>
+                                    <span class="info-box-number">40000</span>
+                                </div>     
+                            </div>
+                        </div>
+                    </a>
+                {{----------- end box -----------}}
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+@endsection
