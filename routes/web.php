c56ebe66152d663efe6e6a3b540b7651e23a0b6e
Azkia <azisarf9@gmail.com>
2019-07-30 22:13:41 +0800

    revisi keluhan

--- routes/web.php
+++ routes/web.php
@@ -26,7 +26,7 @@ Route::group(['prefix' => 'officer_crm',  'middleware' => 'is_officer_crm'], fun
     Route::get('/delete/call{call_id}','callController@destroy')->name('destroy.call');
     Route::get('/edit/editcall{call_id}','callController@edit')->name('edit.call');
     Route::post('/update/call{call_id}','callController@update')->name('update.call');
-
+    Route::get('/call/exportPDF', 'callController@exportPDF');
 
     //------- laporan visit
     Route::get('/insertvisit', 'VisitController@insert')->name('insert.visit'); //show form insert
@@ -35,6 +35,7 @@ Route::group(['prefix' => 'officer_crm',  'middleware' => 'is_officer_crm'], fun
     Route::get('/delete/visit{visit_id}','VisitController@destroy')->name('destroy.visit');
     Route::get('/edit/editvisit{visit_id}','VisitController@edit')->name('edit.visit');
     Route::post('/update/visit{visit_id}','VisitController@update')->name('update.visit');
+    Route::get('/visit/exportPDF', 'VisitController@exportPDF');
 
     //------- laporan keluhan
     Route::get('/insertkeluhan', 'KeluhanController@insert')->name('insert.keluhan'); //show form insert
@@ -43,15 +44,17 @@ Route::group(['prefix' => 'officer_crm',  'middleware' => 'is_officer_crm'], fun
     Route::get('/delete/keluhan{id_keluhan}','KeluhanController@destroy')->name('destroy.keluhan');
     Route::get('/edit/editkeluhan{id_keluhan}','KeluhanController@edit')->name('edit.keluhan');
     Route::post('/update/keluhan{id_keluhan}','KeluhanController@update')->name('update.keluhan');
+    Route::get('/visit/exportPDF', 'VisitController@exportPDF');
 
     //------- laporan kontrak
-    Route::get('/insert', 'KontrakController@insert')->name('insert.kontrak'); //show form insert
+    Route::get('/insertkontrak', 'KontrakController@insert')->name('insert.kontrak'); //show form insert
     Route::get('/kontrak', 'KontrakController@index')->name('index.kontrak');
     Route::post('/store/kontrak', 'KontrakController@store')->name('store.kontrak');
     Route::get('/delete/kontrak{id_kontrak}','KontrakController@destroy')->name('destroy.kontrak');
     Route::get('/edit/editkontrak{id_kontrak}','KontrakController@edit')->name('edit.kontrak');
     Route::post('/update/kontrak{id_kontrak}','KontrakController@update')->name('update.kontrak');
     Route::get('/mou', 'OfficerController@mou')->name('mou.officer');
+    Route::get('/kontrak/exportPDF', 'KontrakController@exportPDF');
 
 });
 
@@ -128,12 +131,12 @@ Route::group(['prefix' => 'admin',  'middleware' => 'is_admin'], function(){
         Route::get('/visit/exportPDF', 'VisitadminController@exportPDF');
     
         //------- laporan keluhan
-        Route::get('/insertkeluhan', 'KeluhanController@insert')->name('insert.keluhan'); //show form insert
-        Route::get('/keluhan', 'KeluhanController@index')->name('index.keluhan');
-        Route::post('/store/keluhan', 'KeluhanController@store')->name('store.keluhan');
-        Route::get('/delete/keluhan{id_keluhan}','KeluhanController@destroy')->name('destroy.keluhan');
-        Route::get('/edit/editkeluhan{id_keluhan}','KeluhanController@edit')->name('edit.keluhan');
-        Route::post('/update/keluhan{id_keluhan}','KeluhanController@update')->name('update.keluhan');
+        Route::get('/insertkeluhan', 'KeluhanadminController@insert')->name('insert.keluhan'); //show form insert
+        Route::get('/keluhan', 'KeluhanadminController@index')->name('index.keluhan');
+        Route::post('/store/keluhan', 'KeluhanadminController@store')->name('store.keluhan');
+        Route::get('/delete/keluhan{id_keluhan}','KeluhanadminController@destroy')->name('destroy.keluhan');
+        Route::get('/edit/editkeluhan{id_keluhan}','KeluhanadminController@edit')->name('edit.keluhan');
+        Route::post('/update/keluhan{id_keluhan}','KeluhanadminController@update')->name('update.keluhan');
         Route::get('/keluhan/exportPDF', 'KeluhanadminController@exportPDF');
     
         //------- laporan kontrak
