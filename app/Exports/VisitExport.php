46cdfe3e3e93ae38b96f7e6f4c41e50bf447ed1f
Azkia <azisarf9@gmail.com>
2019-08-24 22:23:38 +0700

    revisi excel

--- app/Exports/VisitExport.php
+++ app/Exports/VisitExport.php
@@ -4,8 +4,12 @@ namespace App\Exports;
 
 use App\Visit;
 use Maatwebsite\Excel\Concerns\FromCollection;
+use Maatwebsite\Excel\Concerns\WithHeadings;
+use Maatwebsite\Excel\Concerns\ShouldAutoSize;
+use Maatwebsite\Excel\Concerns\WithEvents;
+use Maatwebsite\Excel\Events\AfterSheet;
 
-class VisitExport implements FromCollection
+class VisitExport implements FromCollection, WithHeadings, ShouldAutoSize, WithEvents
 {
     /**
     * @return \Illuminate\Support\Collection
@@ -13,5 +17,31 @@ class VisitExport implements FromCollection
     public function collection()
     {
         return Visit::all();
+    }
+    public function headings(): array
+    {
+        return [
+            'ID Visit',
+            'Nama Perusahaan',
+            'SPV_PIC',
+            'Tanggal Visit',
+            'Waktu In',
+            'Waktu Out',
+            'PIC Meeted',
+            'Kegiatan',
+            'Created_at',
+            'Update_at'
+        ];
+    }
+    public function registerEvents(): array
+    {
+        return [
+            AfterSheet::class    => function(AfterSheet $event) {
+                $cellRange = 'A1:W1'; // All headers
+                //$spreadsheet = $event->spreadsheet;
+                $event->sheet->getDelegate()->getStyle($cellRange)->getFont()->setSize(14);
+
+            },
+        ];
     }
 }
