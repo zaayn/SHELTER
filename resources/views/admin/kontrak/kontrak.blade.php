2b38c45d7987d76a4b5e3774906c62a8ac221f55
Azkia <azisarf9@gmail.com>
2019-09-05 10:17:47 +0700

    tambah mou jadi ijo

--- resources/views/admin/kontrak/kontrak.blade.php
+++ resources/views/admin/kontrak/kontrak.blade.php
@@ -108,7 +108,7 @@
                                         <a onclick="return confirm('Apakah anda yakin akan menutup kontrak ini ?')" href="{{route('closed.kontrak',$kontrak->id_kontrak)}}" class="btn btn-warning btn-sm">Close</a>
                                         @endif
                                         @if($kontrak->datamou)
-                                        <a href="{{route('insertmou.kontrak',$kontrak->id_kontrak)}}" class="btn btn-info btn-sm">insertmou</span></a>
+                                        <a href="{{route('insertmou.kontrak',$kontrak->id_kontrak)}}" class="btn btn-default btn-sm">Tambah MoU</span></a>
                                         @endif
                                     </td>
                                 </tr>
