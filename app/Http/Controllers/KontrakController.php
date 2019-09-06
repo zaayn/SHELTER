c370a0dd2150f799c9052a0c5c6d14b51a163a42
Unknown <muzayyin937@gmail.com>
2019-09-06 16:50:39 +0700

    apa sih az ?

--- app/Http/Controllers/KontrakController.php
+++ app/Http/Controllers/KontrakController.php
@@ -31,7 +31,7 @@ class KontrakController extends Controller
 
     public function insert()
     {
-        $data['customers'] = customer::where('status','aktif');
+        $data['customers'] = customer::where('status','Aktif');
         return view('officer/insertkontrak',$data);
     }
 
