abd4f415a5dde93f4feb88c1a25e48418709f6bc
Azkia <azisarf9@gmail.com>
2019-12-10 20:47:02 +0700

    fix in report

--- resources/views/admin/kontrak/reminder.blade.php
+++ resources/views/admin/kontrak/reminder.blade.php
@@ -50,16 +50,17 @@
                                 <tbody>
                                     @php
                                         $i=0;
+                                        $a=1;
                                         
                                     @endphp
                                 @foreach($kontraks as $kontrak)
                                 <tr>
-                                    <td>{{ $kontrak->id_kontrak }}</td>
+                                    <td>{{ $a++ }}</td>
                                     
                                     <td>{{ $kontrak->nomor_kontrak}}</td>
-                                    @if($sisa <= 30)
+                                    @if($sisa[$i] <= 30)
                                     <td>{{ $sisa[$i++] }} Hari <i class="fa fa-warning" style="font-size:20px;color:red"></i></td>
-                                    @elseif($sisa > 30)
+                                    @elseif($sisa[$i] > 30)
                                     <td>{{ $sisa[$i++] }} Hari <i class="fa fa-warning" style="font-size:20px;color:yellow"></i></td>
                                     @endif
                                     <td>{{ $kontrak->nama_perusahaan }}</td>
