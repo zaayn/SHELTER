306b8b6ce2c16ee1786c6af9449eb2ed33472f37
Unknown <muzayyin937@gmail.com>
2020-01-07 11:12:26 +0700

    test

--- public/admin_lte/dist/js/admin.js
+++ public/admin_lte/dist/js/admin.js
@@ -1,4 +1,4 @@
-$(document).ready(function(){
+$(document).ready(function() {
     $('#mydatatables2').DataTable({
         "sScrollX": "100%",
         "sScrollXInner": "100%",    
@@ -14,8 +14,7 @@ $(document).ready(function(){
         dateFormat: "H:i",
         time_24hr: true
     });
-})
-$(document).ready(function() {
+
   // Setup - add a text input to each footer cell
   $('#mydatatables thead tr').clone(true).appendTo( '#mydatatables thead' );
   $('#mydatatables thead tr:eq(1) th').each( function (i) {
@@ -45,36 +44,6 @@ $(document).ready(function() {
       buttons: ['copy', 'csv', 'excel', 'pdf', 'print']
   } );
 } );
-$(document).ready(function() {
-    // Setup - add a text input to each footer cell
-    $('#mydatatables3 thead tr').clone(true).appendTo( '#mydatatables3 thead' );
-    $('#mydatatables3 thead tr:eq(1) th').each( function (i) {
-      
-        var title = $(this).text();
-        $(this).html( '<input type="text" placeholder="Search '+title+'" />' );
-  
-        $( 'input', this ).on( 'keyup change', function () {
-            if ( table.column(i).search() !== this.value ) {
-                table
-                    .column(i)
-                    .search( this.value )
-                    .draw();
-            }
-        } );
-    } );
-  
-    var table = $('#mydatatables3').DataTable( {
-        orderCellsTop: true,
-        fixedHeader: true,
-        paging: true,
-        searching: true,
-        "sScrollX": "100%",
-        "sScrollXInner": "100%",    
-        show: true,
-        // dom: 'Bfrtip',
-        buttons: ['copy', 'csv', 'excel', 'pdf', 'print']
-    } );
-  } );
 
 $(function () {
     $('[data-toggle="tooltip"]').tooltip()
