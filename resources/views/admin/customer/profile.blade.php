34b4b3f1fc515bcf181b2639c98f938cfc0bc61b
zayn <muzayyin937@gmail.com>
2020-01-18 08:32:23 +0700

    penyesuaian halaman customer

--- resources/views/admin/customer/profile.blade.php
+++ resources/views/admin/customer/profile.blade.php
@@ -5,7 +5,7 @@
     <div class="col-md-12">
         <div class="panel block">
             <div class="panel-body">
-                <h1>Daftar Customer</h1>
+                <h1>Profil Customer</h1>
                 <ol class="breadcrumb">
                     <li><a href={{asset('/admin/home')}}"><i class="fa fa-dashboard"></i> Home</a></li>
                     <li class="active">Customer</li>
