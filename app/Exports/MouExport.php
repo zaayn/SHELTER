95172a1a3f8fb51878a52af23e38346f3ea7de89
Azkia <azisarf9@gmail.com>
2020-01-23 14:56:00 +0800

    gapake cust

--- app/Exports/MouExport.php
+++ app/Exports/MouExport.php
@@ -18,9 +18,8 @@ class MouExport implements FromCollection, WithHeadings, ShouldAutoSize, WithEve
     public function collection()
     {
         $datamou = DB::table('datamou')
-                ->join('customer','kontrak.kode_customer','=','customer.kode_customer')
                 ->join('kontrak','kontrak.nomor_kontrak','=','datamou.nomor_kontrak')
-                ->select('datamou.no_mou','kontrak.nomor_kontrak','customer.nama_perusahaan','datamou.hc',
+                ->select('datamou.no_mou','kontrak.nomor_kontrak','datamou.hc',
                 'datamou.invoice','datamou.mf','datamou.mf_persen','bpjs_tk_persen','bpjs_tenagakerja',
                 'datamou.bpjs_kes_persen','datamou.bpjs_kesehatan','datamou.jiwasraya','datamou.ramamusa',
                 'datamou.ditagihkan','datamou.diprovisasikan','datamou.overheadcost','datamou.training',
