19ef8f5953eda8e08597f7b58776ed38dd20da6f
Unknown <muzayyin937@gmail.com>
2020-03-12 16:10:32 +0700

    hallo

--- public/admin_lte/dist/js/admin.js
+++ public/admin_lte/dist/js/admin.js
@@ -18,33 +18,33 @@ $(document).ready(function() {
     });
 
     // advance datatable
-    $('#mydatatables3 thead tr').clone(true).appendTo( '#mydatatables3 thead' );
-    $('#mydatatables3 thead tr:eq(1) th').each( function (i) {
+    // $('#mydatatables3 thead tr').clone(true).appendTo( '#mydatatables3 thead' );
+    // $('#mydatatables3 thead tr:eq(1) th').each( function (i) {
         
-      var title = $(this).text();
-      $(this).html( '<input type="text" placeholder="Search '+title+'" />' );
+    //   var title = $(this).text();
+    //   $(this).html( '<input type="text" placeholder="Search '+title+'" />' );
 
-      $( 'input', this ).on( 'keyup change', function () {
-          if ( table.column(i).search() !== this.value ) {
-              table
-                  .column(i)
-                  .search( this.value )
-                  .draw();
-          }
-      } );
-    });
+    //   $( 'input', this ).on( 'keyup change', function () {
+    //       if ( table.column(i).search() !== this.value ) {
+    //           table
+    //               .column(i)
+    //               .search( this.value )
+    //               .draw();
+    //       }
+    //   } );
+    // });
 
-  var table = $('#mydatatables3').DataTable( {
-      orderCellsTop: true,
-      fixedHeader: true,
-      paging: true,
-      searching: true,
-      "sScrollX": "100%",
-      "sScrollXInner": "100%",    
-      show: true,
-      // dom: 'Bfrtip',
-      buttons: ['copy', 'csv', 'excel', 'pdf', 'print']
-  } );
+  // var table = $('#mydatatables3').DataTable( {
+  //     orderCellsTop: true,
+  //     fixedHeader: true,
+  //     paging: true,
+  //     searching: true,
+  //     "sScrollX": "100%",
+  //     "sScrollXInner": "100%",    
+  //     show: true,
+  //     // dom: 'Bfrtip',
+  //     buttons: ['copy', 'csv', 'excel', 'pdf', 'print']
+  // } );
 } );
 
 
