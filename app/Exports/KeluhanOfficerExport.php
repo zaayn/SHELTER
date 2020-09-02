9dd426fccb5dcc9a0fbe767d316f961f7d9c5d11
Azkiatunnisa Fajriyati <azisarf9@gmail.com>
2020-09-02 14:28:34 +0800

    Update KeluhanOfficerExport.php

--- app/Exports/KeluhanOfficerExport.php
+++ app/Exports/KeluhanOfficerExport.php
@@ -2,7 +2,7 @@
 
 namespace App\Exports;
 
-use App\Kontrak;
+use App\Keluhan;
 use Maatwebsite\Excel\Concerns\FromCollection;
 use Maatwebsite\Excel\Concerns\WithHeadings;
 use Maatwebsite\Excel\Concerns\ShouldAutoSize;
