35ef2e5fb562c2e6b722f4d4637643457b9e7aee
Azkiatunnisa Fajriyati <azisarf9@gmail.com>
2020-11-11 09:49:26 +0800

    divide 2 pages pdfmou

--- resources/views/admin/mou/pdfmou.blade.php
+++ resources/views/admin/mou/pdfmou.blade.php
@@ -8,30 +8,66 @@
 	<style type="text/css">
 		table tr td,
 		table tr th{
-			font-size: 4pt;
+			font-size: 7pt;
 		}
+        div.page
+        {
+            page-break-after: always;
+            page-break-inside: avoid;
+        }
 	</style>
 	<center>
 		<h5>Laporan MoU</h4>
 	</center>
- 
+ <div class="page">
 	<table class='table table-bordered'>
 		<thead>
 			<tr>
-            <th>No. MoU</th>
-                                    <th>Nomor Kontrak</th>
-                                    <th>No. Adendum</th>
-                                    <th>HC</th>
-                                    <th>Invoice</th>
-                                    <th>MF</th>
-                                    <th>MF (%)</th>
-                                    <th>Ket. % BPJS Ketenagakerjaan</th>
-                                    <th>Nominal BPJS Ketenagakerjaan</th>
-                                    <th>Ket. % BPJS Kesehatan</th>
-                                    <th>Nominal BPJS Kesehatan</th>
+                <th>No. MoU</th>
+                <th>Nomor Kontrak</th>
+                <th>No. Adendum</th>
+                <th>HC</th>
+                <th>Invoice</th>
+                <th>MF</th>
+                <th>MF (%)</th>
+                <th>Ket. % BPJS Ketenagakerjaan</th>
+                <th>Nominal BPJS Ketenagakerjaan</th>
+                <th>Ket. % BPJS Kesehatan</th>
+                <th>Nominal BPJS Kesehatan</th>
                 <th>Jiwasraya</th>
                 <th>Ramamusa</th>
                 <th>Ditagihkan</th>
+                
+			</tr>
+		</thead>
+		<tbody>
+			@php $i=1 @endphp
+			@foreach($datamou as $datamou)
+			<tr>
+			    <td>{{ $i++ }}</td>
+                <td>{{ $datamou->kontrak->nomor_kontrak }}</td>
+                <td>{{ $datamou->no_adendum }}</td>
+                <td>{{ $datamou->hc }}</td>
+                <td>Rp {{ $datamou->invoice }}</td>
+                <td>Rp {{ $datamou->mf }}</td>
+                <td>{{ $datamou->mf_persen ?$datamou->mf_persen.'%':'' }} </td>
+                <td>{{ $datamou->bpjs_tk_persen ?$datamou->bpjs_tk_persen.'%':''}}</td>
+                <td>{{ $datamou->bpjs_tenagakerja ?'Rp'.$datamou->bpjs_tenagakerja:'' }}</td>
+                <td>{{ $datamou->bpjs_kes_persen ?$datamou->bpjs_kes_persen.'%':'' }}</td>
+                <td>{{ $datamou->bpjs_kesehatan ?'Rp'.$datamou->bpjs_kesehatan:'' }}</td>
+                <td>{{ $datamou->jiwasraya ?'Rp'.$datamou->jiwasraya:'' }}</td>
+                <td>{{ $datamou->ramamusa ?'Rp'.$datamou->ramamusa:'' }}</td>
+                <td>{{ $datamou->ditagihkan }}</td>
+                
+			</tr>
+			@endforeach
+		</tbody>
+	</table>
+</div>
+<div class="page">
+    <table class="table table-bordered">
+        <thead>
+            <tr>
                 <th>Diprovisasikan</th>
                 <th>Overheadcost</th>
                 <th>Training</th>
@@ -42,40 +78,23 @@
                 <th>Devices</th>
                 <th>Chemical</th>
                 <th>Pendaftaran MoU</th>
-			</tr>
-		</thead>
-		<tbody>
-			@php $i=1 @endphp
-			@foreach($datamou as $datamou)
-			<tr>
-			<td>{{ $i++ }}</td>
-                                    <td>{{ $datamou->kontrak->nomor_kontrak }}</td>
-                                    <td>{{ $datamou->no_adendum }}</td>
-                                    <td>{{ $datamou->hc }}</td>
-                                    <td>{{ 'Rp'.number_format($datamou->invoice, 2, ',','.') }}</td>
-                                    <td>{{ 'Rp'.number_format($datamou->mf, 2, ',','.') }}</td>
-                                    <td>{{ $datamou->mf_persen ?$datamou->mf_persen.'%':'' }} </td>
-                                    <td>{{ $datamou->bpjs_tk_persen ?$datamou->bpjs_tk_persen.'%':''}}</td>
-                                    <td>{{ $datamou->bpjs_tenagakerja ?'Rp'.number_format($datamou->bpjs_tenagakerja, 2, ',','.'):'' }}</td>
-                                    <td>{{ $datamou->bpjs_kes_persen ?$datamou->bpjs_kes_persen.'%':'' }}</td>
-                                    <td>{{ $datamou->bpjs_kesehatan ?'Rp'.number_format($datamou->bpjs_kesehatan, 2, ',','.'):'' }}</td>
-                                    <td>{{ $datamou->jiwasraya ?number_format($datamou->jiwasraya, 2, ',','.'):'' }}</td>
-                                    <td>{{ $datamou->ramamusa ?number_format($datamou->ramamusa, 2, ',','.'):'' }}</td>
-                                    <td>{{ $datamou->ditagihkan }}</td>
-                                    <td>{{ $datamou->diprovisasikan }}</td>
-                                    <td>{{ $datamou->overheadcost }}</td>
-                                    <td>{{ $datamou->training }}</td>
-                                    <td>{{ $datamou->tanggal_invoice }}</td>
-                                    <td>{{ $datamou->time_of_payment }}</td>
-                                    <td>{{ $datamou->cut_of_date }}</td>
-                                    <td>{{ $datamou->kaporlap }}</td>
-                                    <td>{{ $datamou->devices }}</td>
-                                    <td>{{ $datamou->chemical }}</td>
-                                    <td>{{ $datamou->pendaftaran_mou }}</td>
-			</tr>
-			@endforeach
-		</tbody>
-	</table>
- 
+            </tr>
+        </thead>
+        <tbody>
+            <tr>
+                <td>{{ $datamou->diprovisasikan }}</td>
+                <td>{{ $datamou->overheadcost }}</td>
+                <td>{{ $datamou->training }}</td>
+                <td>{{ $datamou->tanggal_invoice }}</td>
+                <td>{{ $datamou->time_of_payment }}</td>
+                <td>{{ $datamou->cut_of_date }}</td>
+                <td>{{ $datamou->kaporlap }}</td>
+                <td>{{ $datamou->devices }}</td>
+                <td>{{ $datamou->chemical }}</td>
+                <td>{{ $datamou->pendaftaran_mou }}</td>
+            </tr>
+        </tbody>
+    </table>
+</div>
 </body>
 </html>
